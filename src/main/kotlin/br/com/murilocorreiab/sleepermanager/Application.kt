afade5549dd49add85961cc9f4dfe5ac6bf60b75
Murilo Correia <murilo.correiab@gmail.com>
2020-09-27 19:18:14 -0300

    project: Add detekt and jacoco

--- src/main/kotlin/br/com/murilocorreiab/sleepermanager/Application.kt
+++ src/main/kotlin/br/com/murilocorreiab/sleepermanager/Application.kt
@@ -1,11 +1,11 @@
 package br.com.murilocorreiab.sleepermanager
 
-import io.micronaut.runtime.Micronaut.*
+import io.micronaut.runtime.Micronaut.build
 
+@Suppress("SpreadOperator")
 fun main(args: Array<String>) {
 	build()
 	    .args(*args)
 		.packages("br.com.murilocorreia.sleepermanager")
 		.start()
 }
-
