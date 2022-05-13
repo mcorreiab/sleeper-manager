ee8d56876946cde38ff77c9758048c4a6b7c8804
Murilo Correia <murilo.correiab@gmail.com>
2022-05-13 13:21:37 -0300

    project: Delete unused scripts folder

--- scripts/create_players_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE public.players (
-	id varchar(255) NOT NULL,
-	injury_status varchar(255) NULL,
-	"name" varchar(255) NULL,
-	CONSTRAINT players_pkey PRIMARY KEY (id)
-);
-CREATE INDEX players_name_idx ON public.players ("name");
